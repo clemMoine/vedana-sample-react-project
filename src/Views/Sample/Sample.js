9927d66d014bc3478aa8a30886f181351613d0e9
Clément MOINE <clement.moine@ubidreams.com>
2019-10-03 19:07:24 +0200

    Feature : Sample form
    Create a sample form with callback

--- src/Views/Sample/Sample.js
+++ src/Views/Sample/Sample.js
@@ -1,30 +1,49 @@
 // Libraries
-import React from 'react';
+import React, { Component } from "react";
 
 // Images
-import images from '../../Images';
+import images from "../../Images";
 
-import './Styles/Sample.less';
+// Local components
+import SampleForm from "./Components";
 
-function Sample() {
-  return (
-    <div className="Sample">
-      <header className="Sample-header">
-        <img src={images.logo} className="Sample-logo" alt="logo" />
-        <p>
-          Edit <code>src/Views/Sample/Sample.js</code> and save to reload.
-        </p>
-        <a
-          className="Sample-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./Styles/Sample.less";
+
+class Sample extends Component {
+  /**
+   * Callback: fired when form is submitting
+   */
+  handleSubmitForm = formData => {
+    // Display received form data
+    console.log({ formData });
+  };
+
+  render() {
+    return (
+      <div className="Sample">
+        <header className="Sample-header">
+          <img src={images.logo} className="Sample-logo" alt="logo" />
+          <p>
+            Edit <code>src/Views/Sample/Sample.js</code> and save to reload.
+          </p>
+          <a
+            className="Sample-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+
+          {/* Sample form */}
+          <p>
+            This is a react-form sample
+          </p>
+          <SampleForm onSubmit={this.handleSubmitForm} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Sample;
